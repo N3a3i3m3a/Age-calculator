c6565cb8457297e956c38920c39d3bedb021358b
N3a3i3m3a <nishimwenaima3@gmail.com>
2024-03-14 12:19:56 +0200

    Age calculator's commit

--- age.js
+++ age.js
@@ -3,6 +3,11 @@ userInput.max = new Date().toISOString().split("T")[0];
 let result = document.getElementById("result");
 
 function calculateAge(){
+
+    if(userInput.value ===""){
+        result.innerHTML = "Please, inter your birth date!";
+        return;
+    }
     let birthDate = new Date(userInput.value);
 
     let d1 = birthDate.getDate();
